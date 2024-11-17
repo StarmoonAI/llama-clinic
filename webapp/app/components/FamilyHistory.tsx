8c5e58c90a903b15a439073df889d0fba0003077
akdeb <deb2@illinois.edu>
2024-11-17 03:59:56 +0000

    adding history

--- webapp/app/components/FamilyHistory.tsx
+++ webapp/app/components/FamilyHistory.tsx
@@ -1,18 +1,14 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
 import { useState } from "react";
+import AddFamilyHistory from "./AddFamilyHistory";
 
 export const FamilyHistory = () => {
     const [open, setOpen] = useState(false);
 
     return (
         <div className="flex flex-col gap-4">
-            <Button className="flex flex-row gap-2 w-fit" size="sm">
-                <Plus size={16} />
-                Add family history
-            </Button>
+            <AddFamilyHistory />
         </div>
     );
 };
--- webapp/app/home/track/family/page.tsx
+++ webapp/app/home/track/family/page.tsx
@@ -3,14 +3,10 @@ import { getOpenGraphMetadata } from "@/lib/utils";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "Medical records",
-    ...getOpenGraphMetadata("Medical records"),
+    title: "Family history",
+    ...getOpenGraphMetadata("Family history"),
 };
 
 export default async function Home() {
-    return (
-        <div>
-            <FamilyHistory />
-        </div>
-    );
+    return <FamilyHistory />;
 }
