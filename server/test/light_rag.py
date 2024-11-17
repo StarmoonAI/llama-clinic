dba4b2474700fc35d7281763f226557c171846e6
akdeb <deb2@illinois.edu>
2024-11-17 03:07:05 +0000

    hige groq api key

--- server/test/light_rag.py
+++ server/test/light_rag.py
@@ -26,7 +26,7 @@ async def llm_model_func(
         system_prompt=system_prompt,
         history_messages=history_messages,
         # api_key=os.getenv("UPSTAGE_API_KEY"),
-        api_key="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",
+        api_key=os.getenv("GROQ_API_KEY"),
         base_url="https://api.groq.com/openai/v1",
         **kwargs,
     )
